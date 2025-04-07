c23072220ae1404b90e00e71541cb3f105ebd5d8
luoshenshen <luoshenshen@buaa.edu.cn>
2025-04-07 20:45:20 +0800

    fear: fix problems

--- js_code.py
+++ js_code.py
@@ -18,27 +18,18 @@ def obtain_js(html):
     return href
 
 def verify_code_get(jsname, cookie, time):
-    '''代码不麻烦，主要是分析js花了些时间'''
     url = "https://static.wechat.laixuanzuo.com/template/theme2/cache/layout/" + jsname + ".js"
-
     pattern_js_bg = 'void 0\=\=\=.\&\&\(.\=""\);'
     pattern_js_end = '.\.ajax_get'
     pattern_js = pattern_js_bg + '.*' + pattern_js_end
-
     pattern_js_res = '\+"\&".*\+"\&yzm\="'
-
     exjs = network(url, cookie, time)
-
     funjs = re.search(pattern_js, exjs).group(0)
     funjs = funjs[19:-10]
-
     resjs = re.search(pattern_js_res, exjs).group(0)
     resultcommond = resjs[5:-14]
-
     exjs8 = exjs
-
     docjs = execjs.compile(exjs8 + funjs)
-
     return docjs.eval(resultcommond)
 
 
